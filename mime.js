e9b37fb6e5fb66c4c8cbec61896316cd756605a8
hellfire <yeyan992@gmail.com>
2015-01-03 11:36:41 +0800

    mime type

---
+++ mime.js
@@ -0,0 +1,59 @@
+/**
+The MIT License (MIT)
+
+Copyright (c) 2014 
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+ * mime类型的 map
+ * @ author      hellfire
+ * @ date        2014/12/1
+ * 当请求静态服务器文件的类型 html, css, gif, jpg, png, javascript, json, plain text, 我们会在此文件进行映射
+ */
+exports.types = {
+
+  "css": "text/css",
+
+  "gif": "image/gif",
+
+  "html": "text/html",
+
+  "ico": "image/x-icon",
+
+  "jpeg": "image/jpeg",
+
+  "jpg": "image/jpeg",
+
+  "js": "text/javascript",
+
+  "json": "application/json",
+
+  "pdf": "application/pdf",
+
+  "png": "image/png",
+
+  "svg": "image/svg+xml",
+
+  "swf": "application/x-shockwave-flash",
+
+  "tiff": "image/tiff",
+
+  "txt": "text/plain",
+
+  "wav": "audio/x-wav",
+
+  "wma": "audio/x-ms-wma",
+
+  "wmv": "video/x-ms-wmv",
+
+  "xml": "text/xml"
+
+};
